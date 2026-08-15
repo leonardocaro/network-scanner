ea3fe297e9a5b2a5c6d5821fe403ce4d4c0be2b7
leonardocaro <313877781+leonardocaro@users.noreply.github.com>
2026-08-15 15:40:51 +0200

    Add basic TCP port scanner

---
+++ scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+host = "127.0.0.1"
+
+print(f"Scanning {host}...")
+
+for port in range(7995, 8006):
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    result = sock.connect_ex((host, port))
+
+    if result == 0:
+        print(f"[+] Port {port} is OPEN")
+
+    sock.close()
+
+print("Scan complete.")
