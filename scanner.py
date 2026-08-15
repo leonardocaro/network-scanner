4015d4a37c038bf6cc5527c4cc856c995434d6a0
Leo <313877781+leonardocaro@users.noreply.github.com>
2026-08-15 15:45:13 +0200

    Add comment for local HTTP server port
    Added a comment indicating the local HTTP server port.

--- scanner.py
+++ scanner.py
@@ -4,6 +4,7 @@ host = "127.0.0.1"
 
 print(f"Scanning {host}...")
 
+# local HTTP server is running on port 8000
 for port in range(7995, 8006):
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
